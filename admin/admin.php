65534d73ce16f7735ccea8ebce351fa27036e2a1
stephanegelb <stephanegelb@free.fr>
2016-12-10 14:53:02 +0100

    add table for billets in admin page

--- admin/admin.php
+++ admin/admin.php
@@ -61,13 +61,45 @@
                 </tbody>
             </table>
 
+            
+            
+            
             <h2>liste des billets</h2>
             <?php
             $arrayBillets = $blog->getAllBillets();
             echo count($arrayBillets).' billets<br>';
-            echo '<br><pre>'; print_r($arrayBillets); echo '</pre><br>';
+            echo '<div id="$arrayBillets" style="display:none;"><br><pre>'; 
+            print_r($arrayBillets); 
+            echo '</pre><br></div>';
             ?>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>titre</th>
+                        <th>contenu</th>
+                        <th>date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach($arrayBillets as $billet)
+                    {
+                    ?>
+                    <tr>
+                        <td><?php echo $billet->id;?></td>
+                        <td><?php echo $billet->titre;?></td>
+                        <td><?php echo $billet->contenu;?></td>
+                        <td><?php echo $billet->date_creation_fr;?><td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
 
+            
+            
             <h2>liste des comments</h2>
             <?php
             $arrayComments = $blog->getAllComments();
